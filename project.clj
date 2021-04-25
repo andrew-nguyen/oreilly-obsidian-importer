e5cb34be42f9e289768b7580c18f0e8c103bf244
Andrew Nguyen <andrew@na-consulting.net>
2021-04-25 13:20:56 -0700

    Add deps and update license

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject oreilly-obsidian-importer "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :url "https://github.com/andrew-nguyen/oreilly-obsidian-importer"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [techascent/tech.ml.dataset "6.00-beta-7"]]
   :main ^:skip-aot oreilly-obsidian-importer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
