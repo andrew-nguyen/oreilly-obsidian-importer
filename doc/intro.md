7d92f8a65fbb882bae8c27410c4f5edfde0535be
Andrew Nguyen <andrew@na-consulting.net>
2021-04-25 11:40:16 -0700

    Initial creation of repo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to oreilly-obsidian-importer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/oreilly_obsidian_importer/core_test.clj
@@ -0,0 +1,7 @@
+(ns oreilly-obsidian-importer.core-test
+  (:require [clojure.test :refer :all]
+            [oreilly-obsidian-importer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
